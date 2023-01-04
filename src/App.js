e58009cfbbf193d95fac20585be850f9c308df67
Parth Marfatia <parthmarfatia153@gmail.com>
2023-01-04 23:34:00 +0530

    sign up page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import RegisterPage from "./pages/Register";
+
+// CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import { useFirebase } from "../context/Firebase";
+
+const RegisterPage = () => {
+  const firebase = useFirebase();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Signing up a user...", email, password);
+    const result = await firebase.signupUserWithEmailAndPassword(
+      email,
+      password
+    );
+    console.log("Success", result);
+  };
+
+  return (
+    <div className="container mt-5">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Create Account
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterPage;
